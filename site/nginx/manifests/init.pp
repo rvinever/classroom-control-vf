9e6685153b1d2c34298b21b67ba8883fa093d1e0
rvinever <rvinever@yahoo.com>
2016-09-29 07:22:27 -0700

    lab 18.1 edit2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,6 @@
-class nginx ( $root = undef,
-){
+class nginx ( 
+ $root = undef,
+ ){
  case $::osfamily {
   'redhat','debian' : { 
     $package = 'nginx' 
@@ -8,74 +9,79 @@ class nginx ( $root = undef,
     $docroot = '/var/www'
     $confdir = '/etc/nginx' 
     $logdir = '/var/log/nginx'
-# this will be used if we don't pass in a value
-   $default_docroot = '/var/www' }
+    # this will be used if we don't pass in a value
+    $default_docroot = '/var/www' 
+   }
    'windows' : {
-   $package = 'nginx-service' 
-   $owner = 'Administrator' 
-   $group = 'Administrators'
-   $docroot = 'C:/ProgramData/nginx/html'
-   $confdir = 'C:/ProgramData/nginx' 
-   $logdir = 'C:/ProgramData/nginx/logs'
-# this will be used if we don't pass in a value
-   $default_docroot = 'C:/ProgramData/nginx/html' }
+    $package = 'nginx-service' 
+    $owner = 'Administrator' 
+    $group = 'Administrators'
+    $docroot = 'C:/ProgramData/nginx/html'
+    $confdir = 'C:/ProgramData/nginx' 
+    $logdir = 'C:/ProgramData/nginx/logs'
+    # this will be used if we don't pass in a value
+    $default_docroot = 'C:/ProgramData/nginx/html' 
+   }
    default : {
-       fail("Module ${module_name} is not supported on ${::osfamily}")
-  } 
+    fail("Module ${module_name} is not supported on ${::osfamily}")
+   } 
  }
  # Set the default port for default.conf.epp template
-  $port = '80'
+ $port = '80'
 
-# user the service will run as. Used in the nginx.conf.epp template
-  $user = $::osfamily ? { 
+ # user the service will run as. Used in the nginx.conf.epp template
+ $user = $::osfamily ? { 
   'redhat' => 'nginx', 
   'debian' => 'www-data', 
   'windows' => 'nobody',
-}
+ }
 
-# if $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
+ # if $root isn't set, then fall back to the platform default
+ $docroot = $root ? {
   undef => $default_docroot, 
   default => $root,
-  }
+ }
 
-  File {
-   owner => $owner, group => $group, mode => '0664’, }
+ File {
+  owner => $owner, 
+  group => $group, 
+  mode => '0664’, 
+ }
 
-   package { $package: 
-    ensure => present,
-   }
+ package { $package: 
+  ensure => present,
+ }
 
-  file { [ $docroot, "${confdir}/conf.d" ]: 
-    ensure => directory,
-  }
+ file { [ $docroot, "${confdir}/conf.d" ]: 
+  ensure => directory,
+ }
 
-  file { "${docroot}/index.html":
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
+ file { "${docroot}/index.html":
+  ensure => file,
+  source => 'puppet:///modules/nginx/index.html',
+ }
 
-  file { "${confdir}/nginx.conf":
-   ensure => file,
-   content => epp('nginx/nginx.conf.epp',{
-     user => $user, 
-     confdir => $confdir, 
-     logdir => $logdir, 
-     }),
-    notify => Service['nginx'], 
-   }
+ file { "${confdir}/nginx.conf":
+  ensure => file,
+  content => epp('nginx/nginx.conf.epp',{
+   user => $user, 
+   confdir => $confdir, 
+   logdir => $logdir, 
+  }),
+  notify => Service['nginx'], 
+ }
 
-   file { "${confdir}/conf.d/default.conf": 
-    ensure => file,
-    content => epp('nginx/default.conf.epp', {
+ file { "${confdir}/conf.d/default.conf": 
+  ensure => file,
+  content => epp('nginx/default.conf.epp', {
              port => $port, 
              docroot => $docroot, 
              }),
-    notify => Service['nginx'], 
-    }
+  notify => Service['nginx'], 
+  }
 
-   service { 'nginx': 
-     ensure => running, 
-     enable => true,
-   }
+ service { 'nginx': 
+  ensure => running, 
+  enable => true,
+ }
 }
