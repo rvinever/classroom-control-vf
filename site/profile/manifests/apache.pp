3367eb8c7a6c24ab40d38806b466317cfbe0492b
rvinever <rvinever@yahoo.com>
2016-09-30 08:14:32 -0700

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,6 +1,9 @@
 class profile::apache {
-  include ::apache
-#  apache::vhost { '::fqdn':  port    => '80',  docroot => '/var/www/vhost',  }
+#  include ::apache
+  apache::vhost { '::fqdn':  
+  port    => '80',  
+  docroot => '/var/www/vhost',  
+  }
 
 #  include ::apache::mod::php
 }
