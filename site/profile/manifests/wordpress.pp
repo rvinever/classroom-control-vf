ef65d5028595976644d1ee7ce0fe3aabda1ebf80
rvinever <rvinever@yahoo.com>
2016-09-30 11:19:39 -0700

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -2,7 +2,6 @@ class profile::wordpress {
 
   class { 'wordpress':
     install_dir    => '/var/www/html',
-    wp_multisite   => false,
     wp_siteurl     => 'http://54.218.5.21/port/4016',
   }
 }
