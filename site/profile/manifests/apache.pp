470e3813f031b7239332e1cd55198b9a42f53943
rvinever <rvinever@yahoo.com>
2016-09-30 10:58:50 -0700

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,7 +1,7 @@
 class profile::apache {
   include ::apache
   apache::vhost { $::fqdn:
-    ip      => '127.0.0.1',
+#    ip      => '127.0.0.1',
     port    => '8888',
     docroot => '/var/www/html',
   }
